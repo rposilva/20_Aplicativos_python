c3a5a9c1394d4b1e31e66366afdacd57fdd5dafd
Ramon <rposilva.dev@gmail.com>
2025-04-19 18:59:42 -0300

    Create 2_gerenciador.py
    Início do aplicativo Gerenciador de Despesas Recorrentes.

---
+++ gerenciador_despesas02/2_gerenciador.py
@@ -0,0 +1,7 @@
+# Gerenciador de Orçamento Pessoal
+    # Cálculo de Despesa Recorrentes
+        # Algoritmo para registrar e cálcular despesas mensais fixas (como aluguel, contas de consumo, e etc.).
+    # Previsão de Gastos
+        # Utilizar médias de gastos passados para prever gastos futuros em direntes categorias.
+    # Análise de Economia
+        # Algoritmo que calcula quanto dinheiro pode ser economizado ao final do mês após todas as despesas, seguindo dicas de economia baeadas nos hábitos de consumo.
